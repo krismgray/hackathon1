cffd8b9884ef450dc2bbe6259f6f061e4c61a528
Sam Abdelfattah <samfatth@gmail.com>
2017-09-09 10:13:18 -0600

    Controller Push

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,28 @@
 class CommentsController < ApplicationController
-  def edit
-  end
+  before_action :set_post
 
   def new
+    @comment = Comment.new
   end
+
+  def create
+    @comment = @post.comments.new(comment_params)
+
+    if @comment.save
+      redirect_to current_user_post_path(@post.current_user, @post)
+    else
+      render :new
+    end
+  end
+
+private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body, :author)
+  end
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,58 @@
 class PostsController < ApplicationController
+  # before_action :set_user
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
+    @posts = current_user.posts
   end
 
   def show
   end
 
-  def edit
+  def new
+    @post = current_user.posts.new
+    render partial: 'form'
   end
 
-  def new
+  def edit
+    render partial: 'form'
   end
+
+  def create
+    @post = current_user.posts.new(post_params)
+
+    if @post.save
+      redirect_to user_post_path(current_user, @post)
+    else
+      render partial: 'form'
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to [current_user, @post]
+    else
+      render partial: 'form'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to user_posts_path(current_user)
+  end
+
+  private
+
+    def set_user
+      current_user = User.find(params[:user_id])
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:name, :body)
+    end
+
 end
